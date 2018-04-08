da146fc17d521d4e599c5ac41329192885ed948e
Eric Mellow <ericmellow@gmail.com>
2018-04-08 12:39:12 -0600

    Add styling for current-weather card

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,14 +4,14 @@ import './styles/Current-weather.css'
 const CurrentWeather = () => {
   return(
     <div className = 'CurrentWeather'> 
-      <h1>23°</h1>
+      <h1 className = 'current-temp'>23°</h1>
       <h2> Partly Cloudy </h2>
-      <div> 
+      <div className = 'high-and-low'> 
         <h3>42°</h3>
-        <h3>13°</h3>
+        <h3 className = 'low-temp'>13°</h3>
       </div>
-      <article>hggsckidjaslkcnflkajsclknaslkfcjn</article>
-      <div> 
+      <article>Today's weather is going to be windy af, but it will also be sunny and warm, so that's kinda cool, right?!</article>
+      <div className = 'view-buttons'> 
         <button> 7 Hour forecast </button>
         <button> 10 days</button>
       </div>
