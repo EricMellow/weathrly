c6544f5d74a40cde23a6713c876e33fe975ede1c
Eric Mellow <ericmellow@gmail.com>
2018-04-11 14:33:29 -0600

    Add welcome page

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,51 @@ import CurrentWeather from './CurrentWeather';
 import Card from './Card';
 import './styles/App.css'
 import React, { Component } from 'react';
+import DataCleaner from './DataCleaner';
+import getData from './GetData';
+import Welcome from './Welcome';
+
+const weatherData = new DataCleaner;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      location: 'Denver, CO'
+      welcome: true,
+      location: weatherData.currentLocation,
+      currentTemp: weatherData.currentTemp,
+      highTemperature: weatherData.highTemperature,
+      lowTemperature: weatherData.lowTemperature,
+      currentWeatherLongSummary: weatherData.currentWeatherLongSummary,
+      currentWeatherShortSummary: weatherData.currentWeatherShortSummary
     };
+    this.changeWelcomeState = this.changeWelcomeState.bind(this);
   }
  
+  changeWelcomeState() {
+    this.setState({
+      welcome: false
+    })
+  }
+
   render() {
+    if (this.state.welcome) {
+      return (<Welcome changeWelcomeState = {this.changeWelcomeState}/>)
+    } else {
     return (
       <div className ='App'>
         <Background />
         <Search location = {this.state.location}/>
-        <CurrentWeather />
+        <CurrentWeather 
+          currentTemp = {this.state.currentTemp}
+          highTemperature = {this.state.highTemperature}
+          lowTemperature = {this.state.lowTemperature}
+          currentWeatherLongSummary = {this.state.currentWeatherLongSummary} 
+          currentWeatherShortSummary = {this.state.currentWeatherShortSummary}           
+        />
       </div>
     );
+   }
   }
 }
 
---
+++ src/components/DataCleaner.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import data from './Data'
+
+class DataCleaner extends Component {
+  constructor () {
+    super ()
+    this.currentTemp = data.current_observation.temp_f;
+    this.currentDate = data.current_observation.observation_time_rfc822;
+    this.currentLocation = data.current_observation.display_location.full;
+    this.currentWeatherShortSummary = data.current_observation.weather;
+    this.highTemperature = data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    this.lowTemperature = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+    this.currentWeatherLongSummary = data.forecast.txt_forecast.forecastday[0].fcttext;
+    this.sevenHourForecastHours = data.hourly_forecast.map(hour => hour.FCTTIME.civil);
+    this.sevenHourForecastHours.splice(7);
+    this.sevenHourForecastTemps = data.hourly_forecast.map(day =>  day.temp);
+    this.sevenHourForecastTemps.splice(7);
+    this.sevenHourForecastConditions = data.hourly_forecast.map(day => day.condition)
+    this.sevenHourForecastConditions.splice(7);
+    this.tenDayForecastDayNames = data.forecast.simpleforecast.forecastday.map(day => day.date.weekday_short);
+    this.tenDayForecastIcons = data.forecast.simpleforecast.forecastday.map(day => day.conditions);
+    this.tenDayForecastHighTemps = data.forecast.simpleforecast.forecastday.map(day => day.high.fahrenheit);
+    this.tenDayForecastLowTemps = data.forecast.simpleforecast.forecastday.map(day => day.low.fahrenheit);
+  }
+}
+
+export default DataCleaner;
+
+
