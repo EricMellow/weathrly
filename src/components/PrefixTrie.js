b82763d3cc2135c5a3dce222a4de54270967c8fb
Eric Mellow <ericmellow@gmail.com>
2018-04-15 10:59:19 -0600

    Install prefix trie

---
+++ src/components/PrefixTrie.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+const Node = require('./Node');
+
+class PrefixTree extends Component {
+  constructor() {
+    super();
+    this.root = new Node();
+    this.suggestionArray = [];
+    this.wordCount = 0;
+  }
+
+  insert(word, currentNode = this.root) {
+    if (typeof word !== 'string') {
+      return null;
+    }
+
+    word = word.toLowerCase();
+
+    if (!word.length) {
+      if (!currentNode.isAWord) {
+        currentNode.isAWord = true;
+        this.wordCount++;
+        return;
+      }
+      return;
+    }
+
+    if (!currentNode.childrenObj[word[0]]) {
+      currentNode.childrenObj[word[0]] = new Node();
+      currentNode.childrenObj[word[0]].value = word[0];
+    }
+
+    currentNode = currentNode.childrenObj[word[0]];
+    this.insert(word.substr(1), currentNode);
+  }
+
+  suggest(word, currentNode = this.root) {
+    if (typeof word !== 'string') {
+      return null;
+    }
+
+    this.suggestionArray = [];
+    word = word.toLowerCase();
+
+    for (let i = 0; i < word.length; i++) {
+      if (currentNode.childrenObj[word[i]]) {
+        currentNode = currentNode.childrenObj[word[i]];
+      } else {
+        return null;
+      }
+    }
+
+    this.getSuggestions(word, currentNode);
+
+    let sortedObjectsArray = this.suggestionArray.sort((a, b) => b.value - a.value);
+    let cleanArray = sortedObjectsArray.map(object => object.word)
+    return cleanArray;
+  }
+
+  getSuggestions(prefix, currentNode) {
+    if (currentNode.isAWord) {
+      currentNode.weight++;
+      this.suggestionArray.push({ word: prefix, value: currentNode.weight });
+    }
+
+    let letters = Object.keys(currentNode.childrenObj);
+    letters.forEach(letter => {
+      return this.getSuggestions(prefix + letter, currentNode.childrenObj[letter]);
+    });
+  }
+
+  select(word) {
+    let currentNode = this.root;
+    for (let i = 0; i < word.length; i++) {
+      if (currentNode.childrenObj[word[i]]) {
+        currentNode = currentNode.childrenObj[word[i]];
+      }
+    }
+    currentNode.weight++;
+  }
+
+  populate(wordsArray) {
+    if (!Array.isArray(wordsArray)) {
+      return null;
+    }
+    wordsArray.forEach(word => this.insert(word));
+  }
+
+  count() {
+    return this.wordCount;
+  }
+}
+
+module.exports = PrefixTree;
