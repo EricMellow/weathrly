0b4a09c3fe7c61c66e34cdc0f5825a8d5a1e3e0c
sospinar21 <sospinar21@gmail.com>
2018-04-15 12:33:34 -0600

    delete logo, and fix local storage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './styles/Search.css'
 import DateCleaner from './DateCleaner'
+import PrefixTrie from './Prefixtrie'
+import cities from './largest1000cities'
+import Suggestions from './Suggestions'
 
 class Search extends Component {
   constructor (props) {
     super()
     this.state = {
-      userInput: ''
+      userInput: '',
+      prefixTrie: new PrefixTrie()
     }
+    this.state.prefixTrie.populate(cities)
+    this.state.prefixTrie.suggest(this.state.userInput)
     this.captureInfo = this.captureInfo.bind(this)
   }
 
@@ -22,6 +28,7 @@ class Search extends Component {
         <div className = 'searchIcon'/>
         <input className = 'searchInput' 
           type='text' 
+          list='cities'
           placeholder = {`${this.props.location}`} 
           onKeyUp={(event) => {
             if(event.key === 'Enter') {
@@ -30,6 +37,7 @@ class Search extends Component {
             this.captureInfo(event) 
           }}
         />
+        {Suggestions(this.state.prefixTrie.suggestionArray)}
         <h3> 
           {DateCleaner(this.props.date)}
         </h3>
