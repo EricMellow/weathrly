5049d76be0c57e4b20f297edd7501a7fa33939b4
Eric Mellow <ericmellow@gmail.com>
2018-04-15 20:15:31 -0600

    Add card tests

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ class Card extends Component {
     if (this.props.day) {
       return (
         <div className = 'Card'> 
-          <h3>{this.props.day}</h3>
+          <h3 className = 'day'>{this.props.day}</h3>
           <img src={`https://icons.wxug.com/i/c/i/${this.props.icon}.gif`}></img>
           <div className = 'high-low'>
             <h4 className = 'high'>{this.props.highTemp}°</h4>
@@ -21,7 +21,7 @@ class Card extends Component {
     } else {
       return (
         <div className='Card'>
-          <h3>{this.props.hour}</h3>
+          <h3 className = 'hour'>{this.props.hour}</h3>
           <img src={`https://icons.wxug.com/i/c/i/${this.props.icon}.gif`}></img>
           <div className='high-low'>
             <h4 className='high'>{this.props.temp}°</h4>
---
+++ src/test/Card.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from '../components/Card';
+
+
+describe('Card', () => {
+  
+  it('should exist', () => {
+    const cardComponent = shallow(<Card />);
+    
+    expect(cardComponent).toBeDefined();
+  });
+  
+  it('should render as expected when given props', () => {
+    const props = { key: 1, hour: '3:00 PM', icon: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif", temp: 47};
+    const card = shallow(<Card  {...props} />);
+    expect(card.find('.high').text()).toEqual("47°");
+    expect(card.find('.hour').text()).toEqual("3:00 PM");
+  });
+  
+  it('should render a different card when given different props', () => {
+    const props = { key: 1, day: 'FRI', icon: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif", highTemp: 67, lowTemp: 45 };
+    const card = shallow(<Card  {...props} />);
+    expect(card.find('.low').text()).toEqual("45°");
+    expect(card.find('.day').text()).toEqual("FRI");
+  });
+
+})
