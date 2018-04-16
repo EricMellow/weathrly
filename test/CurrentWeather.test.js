012ebb6b73a95555830b335ef6bbd4058ae1ea83
sospinar21 <sospinar21@gmail.com>
2018-04-15 20:00:49 -0600

    CurrentWeatherTests

--- test/CurrentWeather.test.js
+++ test/CurrentWeather.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import CurrentWeather from '../src/components/CurrentWeather';
+import CurrentWeatherCard from '../src/components/CurrentWeatherCard';
 
 
 describe('CurrentWeather', () => {
@@ -24,12 +25,24 @@ describe('CurrentWeather', () => {
     expect(CurrentWeatherComponent.state('tenDayClicked')).toEqual(expectation)
   });
 
+  it('should toggle the TenDay state on click ', () => {
+    const expectation = true
+    const currentState = false
+    CurrentWeatherComponent.setState({tenDayClicked: currentState})
+    CurrentWeatherComponent.instance().showSevenHour()
+    expect(CurrentWeatherComponent.state('sevenHourClicked')).toEqual(expectation)
+  });
+
   it('should toggle the sevenHourClicked state on click ', () => {
     const expectation = true
     const currentState = false
     CurrentWeatherComponent.setState({sevenHourClicked: currentState})
-    CurrentWeatherComponent.instance().showSevenHour()
+    CurrentWeatherComponent.instance().showTen()
     expect(CurrentWeatherComponent.state('tenDayClicked')).toEqual(expectation)
   });
+
+  it('should render the currentWeatherCard component', () => {
+    expect(CurrentWeatherComponent.find(CurrentWeatherCard))
+  })
 
 })
